cfeb38d54ada06ea8e7a20f9a9becd9ab7bc3e4f
Abdulaziz Shaikh <abdulaziz.shaikh1206@gmail.com>
2024-04-04 02:07:59 +0530

    Initialize express app

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import "reflect-metadata";
 import { AppDataSource } from "./config/database";
+import express from "express";
 
 const main = async () => {
   await AppDataSource.initialize();
+  const app = express();
+  app.listen(4000, () => console.log("Server started on localhost:4000"));
 };
 
 main();
